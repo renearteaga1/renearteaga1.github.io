68e3efbeb4697dc5d0efe80717d041e25fa0040a
Rene <renev88@gmail.com>
2021-09-27 17:55:15 -0500

    Projects page updated

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ window.addEventListener('scroll', () => {
   document.querySelector('.hero-title').classList.add('scrolled-title');
   document.querySelector('.preambule__text').classList.add('show');
 
-  document.querySelector('.bio-element').style.transform = 'translateY('+(((scrolled/scrollable)*100)-69)+'%)';
+  if (document.querySelector('.bio-element') != null) {
+    document.querySelector('.bio-element').style.transform = 'translateY('+(((scrolled/scrollable)*100)-69)+'%)';    
+  }
+ 
   // console.log(document.querySelector('.bio-element').style.transform('translateY'))
   if (Math.ceil(scrolled) < 55) {
     console.log('inicio');
     document.querySelector('.hero-color').classList.remove('animate');
     document.querySelector('.hero-title').classList.remove('scrolled-title');
     document.querySelector('.preambule__text').classList.remove('show');
+    document.querySelector('.flecha').style.transform = 'rotate(360deg)';
   }
   // console.log(scrolled);
   if (Math.ceil(scrolled) === scrollable) {
     console.log('fondo');
+    document.querySelector('.flecha').style.transform = 'rotate(180deg)';
   }
+
 })
